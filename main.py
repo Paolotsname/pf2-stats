4412c3f615d3cd1975cf7ff3e6da59bdac66ffee
Paolotsname <paulo.nery97@gmail.com>
2024-12-04 22:33:21 -0300

    I guess this is oop now

--- main.py
+++ main.py
@@ -1,3 +1,71 @@
+from enum import auto, Enum
+from dataclasses import dataclass
+
+
+_list = {"a": [0, 1], "b": [2, 3]}
+
+
+@dataclass
+class Player:
+    name: str
+    level: int
+    armorBonus: int = 0
+    saveBonus: int = 0
+
+    def __post_init__(self):
+        self.ac = _list[name][level] + armorBonus
+        self.fort = _list[name][level] + saveBonus
+        self.refl = _list[name][level] + saveBonus
+        self.will = _list[name][level] + saveBonus
+
+
+@dataclass
+class Enemy:
+    level: int
+    armorBonus: int = 0
+    saveBonus: int = 0
+
+    def __post_init__(self):
+        self.ac = _list[name][level] + armorBonus
+        self.fort = _list[name][level] + saveBonus
+        self.refl = _list[name][level] + saveBonus
+        self.will = _list[name][level] + saveBonus
+
+
+def all_get_rates(
+    playerclass, level, attributes, bonuses, flags={"agile": False, "finesse": False}
+):
+    player = playerclass.getProficiencies(level)
+    enemy = enemyModes[level - 1]
+
+    if flags["agile"]:
+        if flags["finesse"]:
+            weapon_rates0 = get_rates(player.weapon + attributes["str"], enemy.ac)
+            weapon_rates1 = get_rates(player.weapon - 4 + attributes["str"], enemy.ac)
+            weapon_rates2 = get_rates(player.weapon - 8 + attributes["str"], enemy.ac)
+        else:
+            weapon_rates0 = get_rates(player.weapon + attributes["dex"], enemy.ac)
+            weapon_rates1 = get_rates(player.weapon - 5 + attributes["dex"], enemy.ac)
+            weapon_rates2 = get_rates(player.weapon - 10 + attributes["dex"], enemy.ac)
+
+    spell_ac_rates = get_rates(player.spell + attributes[scm], enemy.ac)
+    spell_fort_rates = get_rates(player.spell + attributes[scm], enemy.fort + 10)
+    spell_ref_rates = get_rates(player.spell + attributes[scm], enemy.ref + 10)
+    spell_will_rates = get_rates(player.spell + attributes[scm], enemy.will + 10)
+
+    ac_rates = get_rates(enemy.normal_attack, player.ac)
+    fort_rates = get_rates(enemy.spell_attack, player.fort + 10)
+    ref_rates = get_rates(enemy.spell_attack, player.ref + 10)
+    will_rates = get_rates(enemy.spell_attack, player.will + 10)
+
+    return weapon_rates, spell_rates
+
+
+attributes = {"str": 0, "dex": 0, "con": 0, "int": 0, "wis": 0, "cha": 0}
+
+bonuses = {"circumstance": 0, "item": 0, "status": 0}
+
+
 def clamp(n, min, max):
     if n < min:
         return min
@@ -7,106 +75,60 @@ def clamp(n, min, max):
         return n
 
 
-prof = 11.1
-ac = 21
-
-crit_fail = 0
-fail = 0
-hit = 0
-crit_hit = 0
-
-# 1
-# clutch?
-if prof + 1 >= ac + 10:
-    hit += 5
-elif prof + 1 >= ac:
-    fail = +5
-else:
-    crit_fail += 5
-# 2-19
-for x in range(18):
-    if prof + 2 + x >= ac + 10:
-        crit_hit += 5
-    elif prof + 2 + x >= ac:
-        hit += 5
-    elif prof + 2 + x <= ac - 10:
-        crit_fail += 5
-    else:
-        fail += 5
-# 20
-if prof + 20 >= ac:
-    crit_hit += 5
-elif prof + 20 >= ac - 10:
-    hit += 5
-else:
-    fail += 5
-
-print(f"crit hit:{crit_hit}%")
-print(f"hit:{hit}%")
-print(f"fail:{fail}%")
-print(f"crit fail:{crit_fail}%")
-print()
-
 # a minToHit value of 11.2 means that when the dice rolls 11
 # there's a 20% chance of it being a failure
 # and 80% chance of it being a hit
 # so it would be a total of 59% chance to hit before subtracting critical hits
+def get_rates(prof, ac):
+    # 2-19
+    diceFacesUsed = 0
 
-# 2-19
-diceFacesUsed = 0
+    # defender's critical ac minus attacker's modifier and a +1 to create an offset and start at a dice value 2
+    minToCrit = (ac + 10) - prof + 1
+    sidesThatCritHit = clamp(21 - minToCrit, 0, 18 - diceFacesUsed)
+    diceFacesUsed += sidesThatCritHit
 
-# defender's critical ac minus attacker's modifier and a +1 to create an offset and start at a dice value 2
-minToCrit = (ac + 10) - prof + 1
-sidesThatCritHit = clamp(21 - minToCrit, 0, 18 - diceFacesUsed)
-diceFacesUsed += sidesThatCritHit
+    minToHit = (ac) - prof + 1
+    sidesThatHit = clamp(21 - minToHit - diceFacesUsed, 0, 18 - diceFacesUsed)
+    diceFacesUsed += sidesThatHit
 
-minToHit = (ac) - prof + 1
-sidesThatHit = clamp(21 - minToHit - diceFacesUsed, 0, 18 - diceFacesUsed)
-diceFacesUsed += sidesThatHit
+    minToFail = (ac - 9) - prof + 1
+    sidesThatFail = clamp(21 - minToFail - diceFacesUsed, 0, 18 - diceFacesUsed)
+    diceFacesUsed += sidesThatFail
 
-minToFail = (ac - 9) - prof + 1
-sidesThatFail = clamp(21 - minToFail - diceFacesUsed, 0, 18 - diceFacesUsed)
-diceFacesUsed += sidesThatFail
+    sidesThatCritFail = 18 - diceFacesUsed
 
-sidesThatCritFail = 18 - diceFacesUsed
+    Nat1Value = prof + 1
+    if Nat1Value >= ac + 10:
+        value = clamp(Nat1Value - ac + 10 + 1, 0, 1)
+        sidesThatHit += value
+        sidesThatFail += 1 - value
+    elif Nat1Value >= ac:
+        value = clamp(Nat1Value - ac + 1, 0, 1)
+        sidesThatFail += value
+        sidesThatCritFail += 1 - value
+    else:
+        sidesThatCritFail += 1
+    # 20
+    Nat20Value = prof + 20
+    if Nat20Value + 1 >= ac:
+        value = clamp(Nat20Value - ac + 1, 0, 1)
+        sidesThatCritHit += value
+        sidesThatHit += 1 - value
+    elif Nat20Value + 1 > ac - 10:
+        value = clamp(Nat20Value - ac + 10, 0, 1)
+        sidesThatHit += value
+        sidesThatFail += 1 - value
+    else:
+        sidesThatFail += 1
 
-print(round(sidesThatCritHit, 3))
-print(round(sidesThatHit, 3))
-print(round(sidesThatFail, 3))
-print(round(sidesThatCritFail, 3))
+    return (sidesThatCritFail, sidesThatFail, sidesThatHit, sidesThatCritHit)
 
-# 1
-Nat1Value = prof + 1
-if Nat1Value >= ac + 10:
-    value = clamp(Nat1Value - ac + 10 + 1, 0, 1)
-    sidesThatHit += value
-    sidesThatFail += 1 - value
-elif Nat1Value >= ac:
-    value = clamp(Nat1Value - ac + 1, 0, 1)
-    sidesThatFail += value
-    sidesThatCritFail += 1 - value
-else:
-    sidesThatCritFail += 1
-# 20
-Nat20Value = prof + 20
-if Nat20Value >= ac:
-    value = clamp(Nat20Value - ac + 1, 0, 1)
-    sidesThatCritHit += value
-    sidesThatHit += 1 - value
-elif Nat20Value >= ac - 10:
-    value = clamp(Nat20Value - ac + 10 + 1, 0, 1)
-    sidesThatHit += value
-    sidesThatFail += 1 - value
-else:
-    sidesThatFail += 1
 
-sidesThatCritHit = round(sidesThatCritHit, 3)
-sidesThatHit = round(sidesThatHit, 3)
-sidesThatFail = round(sidesThatFail, 3)
-sidesThatCritFail = round(sidesThatCritFail, 3)
+prof = 11
+dc = 30.1
 
-print()
-print(sidesThatCritHit)
-print(sidesThatHit)
-print(sidesThatFail)
-print(sidesThatCritFail)
+a, b = all_get_rates(alchemist, 1, attributes, bonuses)
+
+print(a)
+print(b)
